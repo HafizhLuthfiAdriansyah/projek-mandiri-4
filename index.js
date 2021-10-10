2b6e1016e4c0b74af563dfb29c3b87d283c7a655
Hafizh Luthfi A <88813336+HafizhLuthfiAdriansyah@users.noreply.github.com>
2021-10-10 10:11:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const getDataAsync = async () => {
+    let response = await fetch("https://api.github.com/users/HafizhLuthfiAdriansyah");
+    let result = await response.json();
+  
+    const name = document.getElementById("name");
+    const login = document.getElementById("login");
+    const bio = document.getElementById("bio");
+    const avatarUrl = document.getElementById("avatar_url");
+  
+    console.log(result);
+  
+    name.innerHTML = result.name;
+    login.innerHTML = result.login;
+    bio.innerHTML = result.bio;
+    avatarUrl.src = result.avatar_url;
+  };
+  getDataAsync();
